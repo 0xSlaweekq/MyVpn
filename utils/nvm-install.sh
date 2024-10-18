55d7951bfb421e79bc8a3392c1f90cbb1a4a0fd3
Slaweekq <celesik@gmail.com>
2024-10-18 21:51:55 +0700

    fix

--- utils/nvm-install.sh
+++ utils/nvm-install.sh
@@ -5,7 +5,10 @@ echo '#################################################################'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 
-export NVM_DIR=~/.nvm && source ~/.nvm/nvm.sh
+export NVM_DIR="~/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+. "~/.cargo/env"
 
 nvm ls-remote
 VERSION=20.13.1
