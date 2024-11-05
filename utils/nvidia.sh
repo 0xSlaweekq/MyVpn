8e50040439b6f0db7be722feeb67f2519b7c1d20
Slaweekq <celesik@gmail.com>
2024-11-05 11:17:20 +0700

    1

--- utils/nvidia.sh
+++ utils/nvidia.sh
@@ -4,10 +4,28 @@ echo 'Installing Nvidia & other graphics drivers'
 echo '#################################################################'
 cd ~
 # For remove all nvidia drivers
-sudo rm -rf /etc/X11/xorg.conf /etc/X11/xorg.conf-external-display /etc/modprobe.d/nvidia.conf \
-  /etc/modprobe.d/blacklist-nvidia.conf /etc/modprobe.d/blacklist-nvidia-nouveau.conf \
-  /etc/systemd/system/nvidia-persistenced.service
+sudo rm -rf /etc/X11/xorg.conf /etc/X11/xorg.conf-external-display \
+  /etc/modprobe.d/nvidia.conf /etc/modprobe.d/blacklist-nvidia.conf \
+  /etc/modprobe.d/blacklist-nvidia-nouveau.conf \
+  /etc/systemd/system/nvidia-persistenced.service \
+  /lib/modprobe.d/nvidia-installer-* /etc/modprobe.d/nvidia-installer-* \
+  /usr/lib/modprobe.d/nvidia-installer-*
+sudo apt-get remove --purge '^nvidia-.*' -y
 sudo apt autoremove $(dpkg -l *nvidia* |grep ii |awk '{print $2}') -y
+sudo nvidia-installer --uninstall
+sudo update-initramfs -u
+
+sudo apt install -y \
+  linux-headers-$(uname -r) clang gcc make acpid build-essential \
+  ca-certificates dirmngr software-properties-common apt-transport-https \
+  curl dkms libglvnd0 libc-dev freeglut3-dev pkg-config libglvnd-dev \
+  libegl-dev libegl1 libgl-dev libgl1 libx11-dev libxmu-dev libxi-dev \
+  libglu1-mesa-dev libfreeimage-dev libglfw3-dev libgles-dev libgles1 \
+  libglvnd-core-dev libglx-dev libopengl-dev
+
+sudo apt install software-properties-qt # for kde qt, for gnome gtk
+
+
 # Get the release version of Ubuntu => 2404
 RELEASE_VERSION=$(lsb_release -rs | sed 's/\([0-9]\+\)\.\([0-9]\+\)/\1\2/')
 
@@ -24,12 +42,6 @@ sudo apt full-upgrade -y
 sudo apt install software-properties-qt # for kde qt, for gnome gtk
 sudo apt install --reinstall -y xserver-xorg-video-all xserver-xorg-video-nouveau \
   xserver-xorg-video-intel xserver-xorg-video-nvidia-555
-sudo apt install -y \
-  linux-headers-$(uname -r) clang gcc make acpid build-essential \
-  ca-certificates dirmngr software-properties-common apt-transport-https \
-  curl dkms libglvnd-core-dev libglvnd0 libglvnd-dev libc-dev freeglut3-dev \
-  libx11-dev libxmu-dev libxi-dev libglu1-mesa-dev libfreeimage-dev \
-  libglfw3-dev
 sudo apt-key del 7fa2af80
 sudo apt install -y nvidia-driver-555 nvidia-headless-555 nvidia-dkms-555
 sudo apt install -y nvidia-settings nvidia-prime
@@ -43,14 +55,14 @@ sudo apt install -y \
 # Update and upgrade the system again to ensure all packages are installed correctly
 sudo apt update
 # apt list cuda-toolkit-* | grep -v config
-sudo apt install -y cuda
-sudo apt install -y cuda-toolkit nvidia-cuda-toolkit nvidia-gds
-/usr/local/cuda/bin/nvcc --version
+# sudo apt install -y cuda
+# sudo apt install -y cuda-toolkit nvidia-cuda-toolkit nvidia-gds
+# /usr/local/cuda/bin/nvcc --version
 
-echo 'export PATH="/usr/bin:/bin:$PATH/usr/local/cuda/bin\${PATH:+:\${PATH}}"' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}
-' >> ~/.bashrc
-source ~/.bashrc
+# echo 'export PATH="/usr/bin:/bin:$PATH/usr/local/cuda/bin\${PATH:+:\${PATH}}"' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}
+# ' >> ~/.bashrc
+# source ~/.bashrc
 
 sudo prime-select on-demand # nvidia|intel|on-demand|query
 sudo nvidia-xconfig --prime
