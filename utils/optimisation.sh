3af183dd46f7b69914da6c8ef699f7ea8f36ed9e
Slaweekq <celesik@gmail.com>
2024-11-19 22:03:01 +0700

    1

--- utils/optimisation.sh
+++ utils/optimisation.sh
@@ -26,7 +26,10 @@ source ~/.bashrc
 # sudo apt install -y power-profiles-daemon
 # powerprofilesctl set performance && powerprofilesctl list
 # Install packages
-sudo apt install -y gamemode cpufrequtils indicator-cpufreq tlp tlp-rdw
+sudo add-apt-repository -y ppa:linrunner/tlp
+sudo apt update
+sudo apt install -y tlp tlp-rdw tp-smapi-dkms acpi-call-dkms
+sudo apt install -y gamemode cpufrequtils indicator-cpufreq tlp
 
 # Install auto-cpufreq
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
@@ -40,9 +43,31 @@ sudo auto-cpufreq --update
 cd ~ && rm -rf auto-cpufreq
 
 # Install tlp
+sudo sh -c 'echo "
+PLATFORM_PROFILE_ON_AC=performance
+PLATFORM_PROFILE_ON_BAT=balanced
+CPU_ENERGY_PERF_POLICY_ON_AC=performance
+CPU_ENERGY_PERF_POLICY_ON_BAT=balance_power
+CPU_SCALING_GOVERNOR_ON_AC=performance
+CPU_SCALING_GOVERNOR_ON_BAT=ondemand
+CPU_MIN_PERF_ON_AC=5
+CPU_MAX_PERF_ON_AC=100
+CPU_MIN_PERF_ON_BAT=5
+CPU_MAX_PERF_ON_BAT=70
+CPU_BOOST_ON_AC=1
+CPU_BOOST_ON_BAT=0
+DISK_APM_LEVEL_ON_AC="255 255"
+DISK_APM_LEVEL_ON_BAT="255 255"
+START_CHARGE_THRESH_BAT0=40
+STOP_CHARGE_THRESH_BAT0=95
+START_CHARGE_THRESH_BAT1=40
+STOP_CHARGE_THRESH_BAT1=95
+" >> /etc/tlp.conf'
 sudo systemctl enable --now tlp.service
 sudo systemctl start tlp.service
+sudo /etc/init.d/tlp restart
 sudo tlp start
+
 echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils
 sudo systemctl restart cpufrequtils
 
