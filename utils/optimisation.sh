9a9c59e329bc8eb980ba15fac35ad4bf1a7f936f
Slaweekq <celesik@gmail.com>
2024-12-31 20:47:17 +0700

    1

--- utils/optimisation.sh
+++ utils/optimisation.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
 
+
+wget -qO - https://dl.xanmod.org/gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/xanmod-kernel.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/xanmod-kernel.gpg] http://deb.xanmod.org releases main" >> /etc/apt/sources.list.d/xanmod-kernel.list'
+sudo apt update && sudo apt install -y linux-xanmod
+
 sudo dpkg --configure -a
 sudo apt install -y -f
 sudo apt install --fix-broken -y
@@ -38,7 +43,7 @@ sudo systemctl enable --now auto-cpufreq
 sudo systemctl start auto-cpufreq
 sudo systemctl status auto-cpufreq
 sudo auto-cpufreq --update
-cd ~ && rm -rf auto-cpufreq
+# cd ~ && rm -rf auto-cpufreq
 
 # Install tlp
 sudo sh -c 'echo "
@@ -54,11 +59,11 @@ CPU_MIN_PERF_ON_BAT=5
 CPU_MAX_PERF_ON_BAT=70
 CPU_BOOST_ON_AC=1
 CPU_BOOST_ON_BAT=0
-DISK_APM_LEVEL_ON_AC="255 255"
-DISK_APM_LEVEL_ON_BAT="255 255"
-START_CHARGE_THRESH_BAT0=40
+DISK_APM_LEVEL_ON_AC=255 255
+DISK_APM_LEVEL_ON_BAT=255 255
+START_CHARGE_THRESH_BAT0=37
 STOP_CHARGE_THRESH_BAT0=95
-START_CHARGE_THRESH_BAT1=40
+START_CHARGE_THRESH_BAT1=37
 STOP_CHARGE_THRESH_BAT1=95
 " >> /etc/tlp.conf'
 sudo systemctl enable --now tlp.service
@@ -78,3 +83,24 @@ cd ~ && rm -rf ./gamemode
 systemctl --user enable gamemoded && systemctl --user start gamemoded
 sudo chmod +x /usr/bin/gamemoderun
 gamemoded -t
+
+
+sudo apt install -y powertop
+sudo powertop --auto-tune
+sudo systemctl enable fstrim.timer
+
+# Настройка intel_pstate для гибридной архитектуры
+echo "Настройка intel_pstate..."
+if ! grep -q "intel_pstate=enable" /etc/default/grub; then
+    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 intel_pstate=enable"/' /etc/default/grub
+    sudo update-grub
+fi
+
+# Перезагрузка системы
+echo "Скрипт завершён. Рекомендуется перезагрузить систему для применения изменений. Перезагрузить сейчас? (y/n)"
+read -r RESTART
+if [[ $RESTART == "y" || $RESTART == "Y" ]]; then
+    reboot
+else
+    echo "Перезагрузите систему позже для применения всех настроек."
+fi
