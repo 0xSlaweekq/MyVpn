cd0fa18856a7715979388565a9313db92d591f64
Slaweekq <celesik@gmail.com>
2025-03-13 13:00:54 +0700

    1

--- utils/cursor/update-cursor.sh
+++ utils/cursor/update-cursor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPDIR=~/Applications/cursor
+APPDIR=/home/$USER/Applications/cursor
 APPIMAGE_URL="https://downloader.cursor.sh/linux/appImage/x64"
 
 wget -O $APPDIR/cursor.AppImage $APPIMAGE_URL
