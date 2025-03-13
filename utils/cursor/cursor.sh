e0d1596735820d1197547d0a7dd7f7d9249f7cd8
Slaweekq <celesik@gmail.com>
2025-03-13 13:50:35 +0700

    1

--- utils/cursor/cursor.sh
+++ utils/cursor/cursor.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 set -e
 
-if ! [ -f /opt/cursor/cursor.appimage ]; then
+if ! [ -f /opt/cursor.appimage ]; then
     echo "🔹 Installing Cursor AI IDE..."
     sudo apt update
     sudo apt install -y curl
 
     echo "Downloading Cursor AppImage..."
-    sudo curl -L https://downloader.cursor.sh/linux/appImage/x64 -o /opt/cursor/cursor.appimage
-    chmod +x /opt/cursor/cursor.AppImage
-    sudo ln -s /opt/cursor/cursor.AppImage /usr/local/bin/cursor
+    sudo mkdir -p /opt/cursor
+    sudo curl -L https://downloader.cursor.sh/linux/appImage/x64 -o /opt/cursor.appimage
+    sudo chmod +x /opt/cursor.AppImage
+    sudo ln -s /opt/cursor.AppImage /usr/local/bin/cursor
 
     BASE_URL=https://raw.githubusercontent.com/0xSlaweekq/MyVpn/main/utils/cursor
     echo "Downloading Cursor icon..."
-    sudo curl -L $BASE_URL/cursor.png -o /opt/cursor/cursor.png
+    sudo curl -L $BASE_URL/cursor.png -o /opt/cursor.png
 
     echo "🔹 Creating .desktop entry for Cursor..."
     mkdir -p "$HOME/.local/share/applications"
     curl -L $BASE_URL/cursor.desktop -o $HOME/.local/share/applications/cursor.desktop
 
     echo "🔹 Creating update script for Cursor..."
-    sudo curl -L $BASE_URL/update-cursor.sh -o /opt/cursor/update-cursor.sh
-    chmod +x /opt/cursor/update-cursor.sh
+    sudo curl -L $BASE_URL/update-cursor.sh -o /opt/update-cursor.sh
+    sudo chmod +x /opt/update-cursor.sh
 
     echo "🔹 Creating update service for Cursor..."
     sudo curl -L $BASE_URL/update-cursor.service -o /etc/systemd/system/update-cursor.service
@@ -41,7 +42,7 @@ if ! [ -f /opt/cursor/cursor.appimage ]; then
     xdg-mime default cursor.desktop text/x-java
 
     # Set Cursor as default editor for git commit messages
-    git config --global core.editor "/opt/cursor/cursor.appimage --wait"
+    git config --global core.editor "/opt/cursor.appimage --wait"
 
     update-desktop-database "$HOME/.local/share/applications"
 
